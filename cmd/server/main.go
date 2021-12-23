9bbee37d7ad474ce7d2d4b53596366de59d29f99
Connor Hindley <conn.hindley@gmail.com>
2021-12-23 13:43:09 -0700

    Store new posts from messages as they come in.
    Kinda just wiring up. Pretty neat.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/connyay/phlog/mail"
 	"github.com/connyay/phlog/server"
 	"github.com/connyay/phlog/store"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	httpAddr := os.Getenv("HTTP_ADDR")
+	if httpAddr == "" {
+		httpAddr = "0.0.0.0:8080"
 	}
-	store := &store.Mem{}
-	log.Fatal(server.ListenHTTP("0.0.0.0:"+port, store))
+	mailAddr := os.Getenv("MAIL_ADDR")
+	if mailAddr == "" {
+		mailAddr = ":8081"
+	}
+	postStore := &store.Mem{}
+	// postStore.AddPost(store.Post{
+	// 	Title:       "Hello World!",
+	// 	Attachment:  `iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII`,
+	// 	ContentType: "image/png",
+	// })
+	mailReader := mail.RawDogReader{
+		ListenAddr: mailAddr,
+	}
+
+	go func() {
+		for msg := range mailReader.Messages() {
+			var post store.Post
+			for _, part := range msg.Parts {
+				if strings.HasPrefix(part.Type, "text/plain") {
+					post.Title = string(part.Body)
+				}
+				if strings.HasPrefix(part.Type, "image/") {
+					post.Attachment = base64.RawStdEncoding.EncodeToString(part.Body)
+					post.ContentType = part.Type
+				}
+			}
+			log.Printf("Storing post %#v", post)
+			err := postStore.AddPost(post)
+			if err != nil {
+				log.Printf("Failed storing post %v %v", post, err)
+			}
+		}
+	}()
+	log.Fatal(server.ListenHTTP(httpAddr, postStore))
 }
--- server/server.go
+++ server/server.go
@@ -16,11 +16,6 @@ var resources embed.FS
 var t = template.Must(template.ParseFS(resources, "templates/*"))
 
 func ListenHTTP(addr string, postStore store.Store) error {
-	postStore.AddPost(store.Post{
-		Title:       "Hello World!",
-		Attachment:  `iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII`,
-		ContentType: "image/png",
-	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := map[string]interface{}{
 			"Env": os.Environ(),
